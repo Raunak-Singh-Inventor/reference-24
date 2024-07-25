a9779cba09e63e6432cde1513014dd1d7f773bbe
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2024-07-25 15:07:19 -0500

    Updated fw_protect.py to add metadata for testing purposes

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -28,7 +28,10 @@ def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
     with open(infile, "rb") as fp:
         firmware = fp.read()
-
+        
+    # Pack version and size into two little-endian shorts
+    metadata = p16(version, endian='little') + p16(len(firmware), endian='little')
+    
     # Append null-terminated message to end of firmware
     firmware_and_message = firmware + message.encode() + b"\00"
 
@@ -39,7 +42,7 @@ def protect_firmware(infile, outfile, version, message):
     signature = signer.sign(h)
 
     # Add together firmware and message along with signature to make the firmware blob
-    firmware_blob = firmware_and_message + signature
+    firmware_blob = metadata + firmware_and_message + signature
 
     # Write firmware blob along with signature to outfile
     with open(outfile, "wb+") as outfile:
