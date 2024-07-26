55529e65402ce0573111c336f005a61c5c159db3
Lin Jiang <lnjng07@protonmail.com>
2024-07-26 16:16:08 -0400

    build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,6 @@
-python3 bl_build.py
+python bl_build.py
 sudo lm4flash ../bootloader/bin/bootloader.bin
-python3 fw_protect.py --infile ../firmware/bin/firmware.bin --outfile firmware_protected.bin --version 2 --message "Firmware V2"
+python fw_protect.py --infile ../firmware/bin/firmware.bin --outfile firmware_protected.bin --version 2 --message "Firmware V2"
 echo "press RESET..."
 sleep 2
-sudo python3 fw_update.py --firmware ./firmware_protected.bin
+sudo python fw_update.py --firmware ./firmware_protected.bin
