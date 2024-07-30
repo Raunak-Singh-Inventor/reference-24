b718b0fd635e345c34e5ac2f591fcb4dcfd64228
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2024-07-30 14:41:23 -0500

    Update build.sh to remove unnecessary sudo

--- tools/build.sh
+++ tools/build.sh
@@ -4,5 +4,5 @@ sudo lm4flash ../bootloader/bin/bootloader.bin
 python fw_protect.py --infile ../firmware/bin/firmware.bin --outfile firmware_protected.bin --version 2 --message "Firmware V2"
 echo "press RESET..."
 sleep 2
-sudo python fw_update.py --firmware ./firmware_protected.bin
+python fw_update.py --firmware ./firmware_protected.bin
 
